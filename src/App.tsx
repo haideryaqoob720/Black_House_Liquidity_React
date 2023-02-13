acc9a71db37170ff64fc6534cc6c63557cad0489
Haider Yaqoob <49457040+haideryaqoob720@users.noreply.github.com>
2023-02-13 18:56:26 +0500

    upladed all code of liquidity and landing page
    uploaded all code of liquidity and landing page with separate CSS

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+// Images
+
+// Components
+import LandingPage from "./components/LandingPage";
+import BlockhouseLiquidity from "./components/BlockhouseLiquidity";
+
+// CSS
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/liquidity" element={<BlockhouseLiquidity />} />
+        </Routes>
+      </BrowserRouter> */}
+    </>
+  );
+}
+
+export default App;
